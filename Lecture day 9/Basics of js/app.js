9c8e5cf962046ecb98855938abb0052cac8ccd98
RahulPraj <rahul971801@gmail.com>
2024-06-11 04:57:38 +0530

    js start

---
+++ Lecture day 9/Basics of js/app.js
@@ -0,0 +1,90 @@
+// console.log("hello bhai log")
+
+
+// String
+
+// let myFirstName = 'Rahul';
+// let myLastName = 'Prajapati';
+// let space = " ";
+
+// console.log(myFirstName+ space + myLastName);
+// console.log(myFirstName + " " + myLastName);
+// console.log(`my first name is ${myFirstName} and my last name is ${myLastName}`);
+
+
+// console.log(myLastName.length);
+
+// method
+// console.log(myFirstName.toUpperCase());
+
+// let collegeName = 'SHARDA';
+
+// console.log(collegeName.toLowerCase());
+
+// trim
+
+// let naam = "      prajapati     ";
+// console.log(typeof(naam));
+// console.log(naam.length);
+// console.log(naam.trim());
+
+
+// Number
+
+// let age = 28;
+// console.log(typeof(age));
+
+// let sum = 1 + 1;
+// let div = 2/3;
+// console.log(sum);
+// console.log(div);
+
+
+//undefined
+// let umar;
+// console.log(umar);
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+// boolean
+// let myAge = true;
+// console.log(typeof(myAge));
+
+
+
+
+// let one = 1;
+// let two = 'false';
+
+// let ans1 = (myAge == one);
+// console.log(ans1);
+
+// let ans2 = (myAge === one);
+// console.log(ans2);
+
+
+// Math functions
+
+// console.log(Math.PI);
+// console.log(Math.LN2);
+// console.log(Math.max(12, 10, 5, 18, 6));
+
+// console.log(Math.random(10.44999999));
+
+// console.log(Math.ceil(10.64));
+// console.log(Math.ceil(10.5));
+// console.log(Math.floor(10.9));
+
+// condition
+
+let age = 20;
+
+// if(age <= 18){
+//     console.log("school ke bache");
+// }
+
+// else{
+//     console.log("college ke heros");
+// }
+
